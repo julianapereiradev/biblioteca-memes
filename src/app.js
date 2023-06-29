e98540b63855b900c7809413ed8d0816989495f1
juliana <julianapereira.dev@gmail.com>
2023-06-29 16:12:47 -0300

    GET and POST /memes created

--- src/app.js
+++ src/app.js
@@ -1,14 +1,34 @@
 import  express  from "express";
 import cors from 'cors';
 
+// Criação do app:
 const app = express();
 
+// Configurações: 
 app.use(cors())
 app.use(express.json())
 
-app.get("/testememes", (req, res) => {
-    res.send("Funcionou o GET do teste")
+const memes = []
+
+// Funções (endpoints):
+app.get("/memes", (req, res) => {
+    res.status(201).send(memes)
 })
 
+app.post("/memes", (req, res) => {
+    const {description, image, category} = req.body
+
+    if(!description || description === "" || !image || image === "" || !category || category === "") {
+        return res.status(409).send("Preencha todos os campos")
+    }
+
+    memes.push({id: (memes.length + 1), description, image, category})
+    res.status(201).send("POST do meme feito com sucesso!")
+})
+
+// app.get("/memes/random", (req, res) => {
+
+// })
+
 const PORT = 5000
-app.listen(PORT, () => console.log(`Servidor rodando na port ${PORT}}`))
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
